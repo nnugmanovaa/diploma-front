a9adc3f511646830fae5b3a7b6def1425cd18282
wubbalubbadub2 <62470060+wubbalubbadub2@users.noreply.github.com>
2022-02-03 13:14:02 +0600

    wording comments were fixed

--- projects/mfo-app/src/environments/environment.prod.ts
+++ projects/mfo-app/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'https://mobile-pre.pitech.kz/api',
-  ADMIN_URL: 'https://api-stage.pitech.kz/api/admin',
-  LOAN_URL: 'https://loan-stage.pitech.kz/api'
+  API_URL: 'https://mobile.pitech.kz/api',
+  ADMIN_URL: 'https://api.pitech.kz/api/admin',
+  LOAN_URL: 'https://loan.pitech.kz/api'
 };
