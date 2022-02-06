f99ce2c284236a10fe2755479279ed19f29c2d73
akanbair <62468300+akanbair@users.noreply.github.com>
2022-02-06 11:39:23 +0600

    google analytics

--- projects/mfo-app/src/app/app.component.ts
+++ projects/mfo-app/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+declare let gtag: Function;
 
 @Component({
   selector: 'mfo-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'mfo-app';
+
+  constructor(public router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        gtag('config', 'UA-174917766-1',
+          {
+            'page_path': event.urlAfterRedirects
+          }
+        );
+      }
+    }
+    )
+  }
 }
