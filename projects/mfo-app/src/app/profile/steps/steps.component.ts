06a5890d053be5ae9b873af6dc7265034239d9b7
Ilyas <ilyas.sagimbekov@gmail.com>
2022-02-07 17:24:06 +0600

    fixed

--- projects/mfo-app/src/app/profile/steps/steps.component.ts
+++ projects/mfo-app/src/app/profile/steps/steps.component.ts
@@ -242,7 +242,14 @@ export class StepsComponent implements OnInit {
   }
 
   createNewReq(){
-    console.log(this.selectedChoice)
+    // console.log(this.selectedChoice)
+    this.loanID = this.selectedChoice.orderId;
+    this.data.loanAmount = this.selectedChoice.loanAmount;
+    this.data.loanPeriod = this.selectedChoice.loanMonthPeriod;
+    this.showSMSModal = true;
+    this.code = null;
+    this.resultShow = false;
+    this.alternativeResult = false;
   }
 
   strokeLoading(){
