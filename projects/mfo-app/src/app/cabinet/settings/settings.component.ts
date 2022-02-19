bfa8e5122b8f35d9bef9bbf5ce3ed28d62763764
akanbair <62468300+akanbair@users.noreply.github.com>
2022-02-19 13:49:41 +0600

    index disabled in settings

--- projects/mfo-app/src/app/cabinet/settings/settings.component.ts
+++ projects/mfo-app/src/app/cabinet/settings/settings.component.ts
@@ -16,7 +16,7 @@ export class SettingsComponent implements OnInit {
     "addressInfoDto": {
         "region": "",
         "city": "",
-        "postalCode": "",
+        // "postalCode": "",
         "street": "",
         "house": "",
         "apartment": "",
