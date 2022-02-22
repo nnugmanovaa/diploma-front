3ad7b6c96fc3e1e69da4cea40c048262304c2db9
akanbair <62468300+akanbair@users.noreply.github.com>
2022-02-22 12:07:19 +0600

    whatsapp and telegram added, thanks Hack

--- projects/mfo-app/src/app/app.component.ts
+++ projects/mfo-app/src/app/app.component.ts
@@ -1,27 +1,44 @@
 import { Component } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-
-declare let gtag: Function;
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
 
 @Component({
   selector: 'mfo-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('popOverState', [
+      state('show', style({
+        opacity: 1
+      })),
+      state('hide', style({
+        opacity: 0
+      })),
+      transition('show => hide', animate('100ms ease-out')),
+      transition('hide => show', animate('600ms ease-out')),
+    ])
+  ]
 })
-
 export class AppComponent {
+  media: boolean = false;
+  img: string = "call-ico.svg";
+
   title = 'mfo-app';
 
-  constructor(public router: Router) {
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        gtag('config', 'UA-174917766-1',
-          {
-            'page_path': event.urlAfterRedirects
-          }
-        );
-      }
-    }
-    )
+  constructor() { }
+
+  get stateName() {
+    return this.media ? 'show' : 'hide'
+  }
+
+  toggle() {
+    this.media = !this.media;
+    this.img = this.media ? 'close-button.png' : 'call-ico.svg';
   }
 }
